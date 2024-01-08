735ffdbabd91daf8011006e3c9c2b6e977146e75
frarosset <37275581+frarosset@users.noreply.github.com>
2024-01-08 20:20:23 +0100

    Add functions for the basic math operators

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+/* Calculator operations */
+function add(x1,x2){
+    return Number(x1)+Number(x2);
+}
+
+function subtract(x1,x2){
+    return x1-x2;
+}
+
+function multiply(x1,x2){
+    return x1*x2;
+}
+
+function divide(x1,x2){
+    return x1/x2;
+}
+
+
