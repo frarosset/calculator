62a51ec1631aaa1de3899255c99917ccebd15ff2
frarosset <37275581+frarosset@users.noreply.github.com>
2024-01-08 20:26:38 +0100

    Add operate() function

--- js/script.js
+++ js/script.js
@@ -17,3 +17,25 @@ function divide(x1,x2){
 }
 
 
+let x1,x2,operation; 
+
+function operate(operation,x1,x2){
+    let result;
+    switch(operation){
+        case '+':
+            result = add(x1,x2);
+            break;
+        case '-':
+            result = subtract(x1,x2);
+            break;
+        case '*':
+            result = multiply(x1,x2);
+            break;        
+        case '/':
+            result = divide(x1,x2);
+            break;        
+        default:
+            console.log('Invalid operation!');
+    }
+    return result;
+}
